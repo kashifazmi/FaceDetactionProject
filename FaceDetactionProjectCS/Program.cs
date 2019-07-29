04b639c8b75a7a3f5bdb1cc94b4efa1f7ffbb31a
mohd.kashif <mohd.kashif.ext@siemens.com>
2019-07-29 15:50:58 +0530

    Add project files.

---
+++ FaceDetactionProjectCS/Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using Microsoft.Azure.CognitiveServices.Vision.Face;
+using Microsoft.Azure.CognitiveServices.Vision.Face.Models;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+
+
+namespace FaceDetactionProjectCS
+{
+    class Program
+    {
+        // subscriptionKey = "0123456789abcdef0123456789ABCDEF"
+        private const string subscriptionKey = "9817dd19b32148f7b49f90d7fc662336";
+        private const string faceEndpoint = "https://westcentralus.api.cognitive.microsoft.com";
+        // localImagePath = @"C:\Documents\LocalImage.jpg"
+        private const string localImagePath = @"D:\personal\PIC\surprise.jpeg";
+        private const string remoteImageUrl ="https://upload.wikimedia.org/wikipedia/commons/3/37/Dagestani_man_and_woman.jpg";
+
+        private static readonly FaceAttributeType[] faceAttributes =
+            { FaceAttributeType.Age, FaceAttributeType.Gender, FaceAttributeType.Emotion, FaceAttributeType.Smile };
+        static void Main(string[] args)
+        {
+            FaceClient faceClient = new FaceClient(
+                new ApiKeyServiceClientCredentials(subscriptionKey),
+                new System.Net.Http.DelegatingHandler[] { });
+            faceClient.Endpoint = faceEndpoint;
+            Console.WriteLine("Faces being detected ...");
+            //var t1 = DetectRemoteAsync(faceClient, remoteImageUrl);
+            var t2 = DetectLocalAsync(faceClient, localImagePath);
+
+            Task.WhenAll(t2).Wait(5000);
+            Console.WriteLine("Press any key to exit");
+            Console.ReadLine();
+        }
+
+        // Detect faces in a remote image
+        private static async Task DetectRemoteAsync(
+            FaceClient faceClient, string imageUrl)
+        {
+            if (!Uri.IsWellFormedUriString(imageUrl, UriKind.Absolute))
+            {
+                Console.WriteLine("\nInvalid remoteImageUrl:\n{0} \n", imageUrl);
+                return;
+            }
+
+            try
+            {
+                IList<DetectedFace> faceList =
+                    await faceClient.Face.DetectWithUrlAsync(
+                        imageUrl, true, false, faceAttributes);
+
+                DisplayAttributes(GetFaceAttributes(faceList, imageUrl), imageUrl);
+            }
+            catch (APIErrorException e)
+            {
+                Console.WriteLine(imageUrl + ": " + e.Message);
+            }
+        }
+
+        // Detect faces in a local image
+        private static async Task DetectLocalAsync(FaceClient faceClient, string imagePath)
+        {
+            if (!File.Exists(imagePath))
+            {
+                Console.WriteLine(
+                    "\nUnable to open or read localImagePath:\n{0} \n", imagePath);
+                return;
+            }
+
+            try
+            {
+                using (Stream imageStream = File.OpenRead(imagePath))
+                {
+                    IList<DetectedFace> faceList =
+                            await faceClient.Face.DetectWithStreamAsync(
+                                imageStream, true, false, faceAttributes);
+                    DisplayAttributes(
+                        GetFaceAttributes(faceList, imagePath), imagePath);
+                }
+            }
+            catch (APIErrorException e)
+            {
+                Console.WriteLine(imagePath + ": " + e.Message);
+            }
+        }
+
+        private static string GetFaceAttributes(
+    IList<DetectedFace> faceList, string imagePath)
+        {
+            string attributes = string.Empty;
+
+            foreach (DetectedFace face in faceList)
+            {
+                double? age = face.FaceAttributes.Age;
+                string gender = face.FaceAttributes.Gender.ToString();
+                string smile = face.FaceAttributes.Smile.ToString();
+                attributes += gender + " " + age + "   " + smile+ " ";
+            }
+
+            return attributes;
+        }
+
+        // Display the face attributes
+        private static void DisplayAttributes(string attributes, string imageUri)
+        {
+            Console.WriteLine(imageUri);
+            Console.WriteLine(attributes + "\n");
+        }
+    }
+}
+    
